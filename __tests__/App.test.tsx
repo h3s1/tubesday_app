147d2f96ad93144c6820322eba27d724c4c9611c
Kong Chae Won <chaewonkong@gmail.com>
2019-10-28 20:58:10 +0900

    feat: add jest and enzyme

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+
+import { shallow } from 'enzyme';
+import React from 'react';
+import App from '../src/components/App';
+import '../tests/setupTest';
+
+const createTestProps = (props: object) => ({
+  ...props,
+});
+
+describe('App', () => {
+  const props = createTestProps({});
+  const wrapper = shallow<App>(<App {...props} />);
+
+  describe('rendering', () => {
+    it('should render a <View />', () => {
+      expect(wrapper.find('View')).toHaveLength(1);
+    });
+  });
+});
