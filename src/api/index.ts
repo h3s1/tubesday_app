72f71a59cfe6500885e5e2882dc72499c05689ff
Kong Chae Won <chaewonkong@gmail.com>
2019-10-30 20:06:14 +0900

    feat: add json-server api

---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+import axios from "axios"
+import {GetArticleRes, GetArticlesRes} from "../shared-interfaces"
+
+const LOCAL_HOST_URL = "http://localhost:3000"
+
+const baseApi = axios.create({
+    baseURL: LOCAL_HOST_URL,
+    timeout: 2000
+})
+
+export const tubesdayApi = {
+    getPost: async (postId: number): Promise<GetArticleRes> => {
+        const res = await baseApi.get(`posts/${postId}`)
+        return res
+    },
+    getAllPosts: async (): Promise<GetArticlesRes> => {
+        const res = await baseApi.get("posts")
+        return res
+    }
+}
