6fe2a8251f98868df3411c132cab156bbb4e059b
Kong Chae Won <chaewonkong@gmail.com>
2019-11-15 00:29:43 +0900

    feat: add react-native-vector-icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { View, Text, Animated } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { DrawerActions } from 'react-navigation-drawer';
+import Icon from 'react-native-vector-icons/Ionicons'
 
 interface IProps {
-  style: {opacity: any};
+  style: { opacity: any };
   navigation: any;
 }
 
@@ -12,21 +13,27 @@ const Header: React.FC<IProps> = props => {
   return (
     <Animated.View
       style={{
-        justifyContent: 'center',
+        justifyContent: 'space-around',
         alignItems: 'center',
+        flexDirection: "row",
         flex: 1,
         backgroundColor: 'red',
         opacity: props.style.opacity || 1,
       }}>
+      <Text><Icon name="ios-menu" size={30} color="white" /></Text>
       <Text
         onPress={() => props.navigation.dispatch(DrawerActions.openDrawer())}
         style={{
           fontSize: 24,
           color: 'white',
-          letterSpacing: 6,
+          letterSpacing: 4,
           textAlign: 'center',
         }}>
         TUBESDAY
+
+      </Text>
+      <Text>
+        <Icon name="ios-search" size={30} color="white" />
       </Text>
     </Animated.View>
   );
