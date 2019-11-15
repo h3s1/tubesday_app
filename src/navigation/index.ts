76e64e0865b4a7f65fdef6c9241c2e80c3ec4052
Kong Chae Won <chaewonkong@gmail.com>
2019-11-15 15:32:19 +0900

    feat: change drawerLabel of CreatePost

--- src/navigation/index.ts
+++ src/navigation/index.ts
@@ -7,21 +7,35 @@ import ListScreen from "../screens/ListScreen"
 import CreatePostScreen from "../screens/CreatePostScreen"
 
 const { width, height } = Dimensions.get("window");
-
+const HEADER_EXPANDED_HEIGHT = Math.min(60, Math.round(height / 10));
 
 const HomeStack = createStackNavigator({
     List: {
         screen: ListScreen,
         navigationOptions: ({navigation}) => ({header:null})
+    },
+    Create: {
+        screen: CreatePostScreen,
+        navigationOptions: ({ navigation }) => ({
+            title: "영상 추천하기",
+            headerStyle: {
+              backgroundColor: "red",
+              borderBottomWidth: 0,
+              height: HEADER_EXPANDED_HEIGHT
+            },
+            headerTintColor: "#fff",
+            headerTitle: "영상 추가하기",
+            headerForceInset: { top: "never" }
+          })
     }
 })
 
+
+
 const Drawer = createDrawerNavigator(
     {
-        Home: HomeStack,
-        Add: {
-            screen: CreatePostScreen,
-        }
+        Home: {screen: HomeStack, navigationOptions: {drawerLabel: "메인화면"}},
+        Create: {screen: CreatePostScreen, navigationOptions: {drawerLabel: "영상 추천하기"}}
     }, 
     {
         initialRouteName: "Home",
